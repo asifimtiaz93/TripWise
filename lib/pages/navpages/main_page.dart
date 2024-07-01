c31ec8c389a1f9345da7dcfab9857c5b29db440e
N M N <nmn.tanveer@gmail.com>
2024-07-02 01:01:10 +0600

    adjust
    initial adjustment

--- lib/pages/navpages/main_page.dart
+++ lib/pages/navpages/main_page.dart
@@ -1,14 +1,11 @@
-
 import 'package:flutter/material.dart';
 import 'package:tripwise/pages/navpages/home_page.dart';
 import 'package:tripwise/pages/navpages/plan_page.dart';
 import 'package:tripwise/pages/navpages/profile_page.dart';
 import 'package:tripwise/pages/navpages/search_page.dart';
 import 'package:tripwise/pages/navpages/vr_page.dart';
-
 import 'package:tripwise/pages/navpages/popular_places.dart';
 import 'package:tripwise/pages/navpages/booking_page.dart';
-
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -27,23 +24,87 @@ class _MainPageState extends State<MainPage> {
     PopularPlacesPage(),
     BookingPage(),
   ];
-  int currentIndex = 0;
+  int bottomNavIndex = 0;
+  int drawerIndex = 0;
 
-  void onTap(int index) {
+  void onBottomNavTap(int index) {
     setState(() {
-      currentIndex = index;
+      bottomNavIndex = index;
     });
+  }
 
+  void onDrawerTap(int index) {
+    setState(() {
+      drawerIndex = index;
+      Navigator.pop(context); // Close the drawer
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: pages[currentIndex],
+      appBar: AppBar(
+        title: Text('App Title'), // You can update the title as needed
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Menu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.house_siding_outlined),
+              title: Text('Home'),
+              onTap: () => onDrawerTap(0),
+            ),
+            ListTile(
+              leading: Icon(Icons.threesixty),
+              title: Text('VR'),
+              onTap: () => onDrawerTap(1),
+            ),
+            ListTile(
+              leading: Icon(Icons.search),
+              title: Text('Search'),
+              onTap: () => onDrawerTap(2),
+            ),
+            ListTile(
+              leading: Icon(Icons.assistant_outlined),
+              title: Text('Plan'),
+              onTap: () => onDrawerTap(3),
+            ),
+            ListTile(
+              leading: Icon(Icons.person_outline_outlined),
+              title: Text('Profile'),
+              onTap: () => onDrawerTap(4),
+            ),
+            ListTile(
+              leading: Icon(Icons.place),
+              title: Text('Popular Places'),
+              onTap: () => onDrawerTap(5),
+            ),
+            ListTile(
+              leading: Icon(Icons.book_online),
+              title: Text('Booking'),
+              onTap: () => onDrawerTap(6),
+            ),
+          ],
+        ),
+      ),
+      body: pages[drawerIndex],
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
-        onTap: onTap,
-        currentIndex: currentIndex,
+        onTap: onBottomNavTap,
+        currentIndex: bottomNavIndex,
         selectedItemColor: Colors.black54,
         unselectedItemColor: Colors.black38,
         elevation: 2,
@@ -60,9 +121,7 @@ class _MainPageState extends State<MainPage> {
               label: 'Plan', icon: Icon(Icons.assistant_outlined)),
           BottomNavigationBarItem(
               label: 'Profile', icon: Icon(Icons.person_outline_outlined)),
-
-       ],
-
+        ],
       ),
     );
   }
